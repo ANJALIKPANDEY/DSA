6e402661cd871e2512156e4c1aa5015ebe35ea08
ANJALI PANDEY <51261859+ANJALIKPANDEY@users.noreply.github.com>
2024-06-04 11:15:31 +0530

    #121_BuySellStock.java

---
+++ #121_BuySellStock.java
@@ -0,0 +1,18 @@
+/**
+*
+*  LC--> #121 Best Time to Buy and Sell Stock
+*
+**/
+class Solution {
+    public int maxProfit(int[] prices) {
+        int mini = prices[0];
+        int profit = 0;
+
+        for(int i=1; i<prices.length; i++) {
+            int cost = prices[i] - mini;
+            profit = Math.max(profit, cost);
+            mini = Math.min(mini, prices[i]);
+        }
+        return profit;
+    }
+}
